166ef7999389ced91c784af7a8e2addfdc578bd3
T7SEN <a.hitelare2@gmail.com>
2026-04-12 12:12:28 +0200

    refactor: clean up comments in next.config.ts for improved clarity

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
+// next.config.ts
 import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
-// Define strict security headers
 const securityHeaders = [
   {
     key: "X-DNS-Prefetch-Control",
@@ -12,12 +12,10 @@ const securityHeaders = [
     value: "max-age=63072000; includeSubDomains; preload",
   },
   {
-    // Prevents Clickjacking
     key: "X-Frame-Options",
     value: "DENY",
   },
   {
-    // Prevents MIME-type sniffing
     key: "X-Content-Type-Options",
     value: "nosniff",
   },
@@ -26,7 +24,6 @@ const securityHeaders = [
     value: "strict-origin-when-cross-origin",
   },
   {
-    // A balanced Content Security Policy protecting against XSS while allowing PostHog
     key: "Content-Security-Policy",
     value:
       "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://eu.i.posthog.com https://eu-assets.i.posthog.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; font-src 'self' data:; object-src 'none'; base-uri 'self'; frame-ancestors 'none'; connect-src 'self' https://eu.i.posthog.com https://eu-assets.i.posthog.com;",
@@ -35,13 +32,9 @@ const securityHeaders = [
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  images: {
-    unoptimized: true,
-  },
   async headers() {
     return [
       {
-        // Apply these headers to all routes in the application
         source: "/(.*)",
         headers: securityHeaders,
       },
@@ -63,38 +56,14 @@ const nextConfig: NextConfig = {
 };
 
 export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
   org: "t7sen-c0",
-
   project: "links",
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
   tunnelRoute: "/monitoring",
-
   webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
     automaticVercelMonitors: true,
-
-    // Tree-shaking options for reducing bundle size
     treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
       removeDebugLogging: true,
     },
   },
